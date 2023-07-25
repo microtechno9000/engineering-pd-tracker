2d3d9dd9cacc8634a4293eb3022bb005d8c1dc9c
Microtechno9000 <microtechno@gmail.com>
2023-07-25 21:47:29 +0930

    branch merge and fix

--- app/records/routes.py
+++ app/records/routes.py
@@ -5,6 +5,17 @@ from app.models.record import Record
 
 
 @blueprint_records.route('/records')
-def media():
+def view_records():
     records = Record.query.all()
     return render_template('records.html', records=records)
+
+
+@blueprint_records.route('/record-add')
+def add_record():
+    return render_template('record_add.html')
+
+
+@blueprint_records.route('/record/<id>')
+def edit_record(id):
+    record = Record.query.filter_by(id=id).first()
+    return render_template('record_edit.html', record=record)
